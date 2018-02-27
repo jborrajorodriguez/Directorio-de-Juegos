f6c1378bbc7688bab5335f3f309eb5b5b9b52beb
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-02-27 08:46:07 +0100

    Metodo mostrarPlataformas añadido

--- src/directoriodejuegos/FuncionesDirectorio.java
+++ src/directoriodejuegos/FuncionesDirectorio.java
@@ -23,9 +23,20 @@ public class FuncionesDirectorio {
         String tipo=IntroducirDatos.introducirString("Que tipo de consola tienes");
         String des=IntroducirDatos.introducirString("Introduce una pequeña descripción");
         int año=IntroducirDatos.introducirInt("Año de la "+nom);
-        
         plataforma.add(new Plataforma(nom,tipo,des,año));
         
+    }
+    /**
+     * Metodo mostrar Plataformas
+     * 
+     * Este metodo muestra todas las plataformas que se encuentren en el ArrayList
+     * @param plataforma 
+     */
+    public void mostrarPlataformas(ArrayList <Plataforma> plataforma){
+        for(int i=0;i<plataforma.size();i++){
+            System.out.println(plataforma.get(i).toString());
+        }
+            
     }
     
 }
