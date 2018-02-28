8d9f7616afc69e3d2bc91e9f195d3a2dc8b6aadb
arturo <arturo@ArturoMSI>
2018-02-28 09:19:53 +0100

    Cambio en los metodos

--- src/directoriodejuegos/FuncionesDirectorio.java
+++ src/directoriodejuegos/FuncionesDirectorio.java
@@ -5,6 +5,7 @@ import com.proyecto.elementos.Juego;
 import com.proyecto.elementos.Plataforma;
 import java.util.ArrayList;
 import com.proyecto.introducirdatos.IntroducirDatos;
+import java.io.File;
 
 /**
  * Clase Funciones del directorio.
@@ -13,12 +14,15 @@ import com.proyecto.introducirdatos.IntroducirDatos;
  */
 public class FuncionesDirectorio{
 
+    private static ArrayList<Juego> juego=new ArrayList();
+    private static ArrayList<Plataforma> plataforma=new ArrayList();
+    private static File ficheroJuegos, ficheroPlataformas;
+
     /**
      * Metodo para añadir un nuevo juego
      *
-     * @param ju ArrayList con los juegos
      */
-    public static void añadirJuego(ArrayList<Juego> ju){
+    public static void añadirJuego(){
         String tit=IntroducirDatos.introducirString("Titulo del juego");
         String des=IntroducirDatos.introducirString("Desarrollador del juego");
         String descr=IntroducirDatos.introducirString("Introduce una descripcion");
@@ -28,30 +32,38 @@ public class FuncionesDirectorio{
         boolean dlc=IntroducirDatos.introducirBoolean("Tiene dlc's");
         boolean coop=IntroducirDatos.introducirBoolean("Tiene modo coperativo?");
         boolean term=IntroducirDatos.introducirBoolean("Has finalizado el juego?");
-        ju.add(new Juego(tit, des, descr, plat, añlan, njug, dlc, coop, term));
+        juego.add(new Juego(tit, des, descr, plat, añlan, njug, dlc, coop, term));
+    }
+
+    public static void crearFicheros(){
+        ficheroJuegos=new File("FJuegos.txt");
+        ficheroPlataformas=new File("FPlataformas.txt");
+
+    }
+
+    public static void guardarFicheros(){
+
     }
 
     /**
      * Metodo para mostrar todos los juegos que se poseen
      *
-     * @param ju ArrayList con los juegos
      */
-    public static void mostrarJuegos(ArrayList<Juego> ju){
-        for(int i=0; i<ju.size(); i++){
-            System.out.println(ju.get(i).toString());
+    public static void mostrarJuegos(){
+        for(int i=0; i<juego.size(); i++){
+            System.out.println(juego.get(i).toString());
         }
     }
 
     /**
      * Metodo para buscar los juegos por titulo
      *
-     * @param ju ArrayList con los juegos
      */
-    public static void buscarTitulo(ArrayList<Juego> ju){
+    public static void buscarTitulo(){
         String titulo=IntroducirDatos.introducirString("Titulo del juego a buscar?");
-        for(int i=0; i<ju.size(); i++){
-            if(titulo.equalsIgnoreCase(ju.get(i).getTitulo())){
-                Display.mostrarMensaje(ju.get(i).toString());
+        for(int i=0; i<juego.size(); i++){
+            if(titulo.equalsIgnoreCase(juego.get(i).getTitulo())){
+                Display.mostrarMensaje(juego.get(i).toString());
             }
         }
     }
@@ -59,13 +71,12 @@ public class FuncionesDirectorio{
     /**
      * Metodo para buscar juegos por su desarrollador
      *
-     * @param ju ArrayList con los juegos
      */
-    public static void buscarDesarrollador(ArrayList<Juego> ju){
+    public static void buscarDesarrollador(){
         String desarr=IntroducirDatos.introducirString("Desarrollador del juego a buscar?");
-        for(int i=0; i<ju.size(); i++){
-            if(desarr.equalsIgnoreCase(ju.get(i).getDesarrollador())){
-                System.out.println(ju.get(i).toString());
+        for(int i=0; i<juego.size(); i++){
+            if(desarr.equalsIgnoreCase(juego.get(i).getDesarrollador())){
+                System.out.println(juego.get(i).toString());
             }
         }
     }
@@ -73,13 +84,12 @@ public class FuncionesDirectorio{
     /**
      * Metodo para buscer juegos segun su plataforma
      *
-     * @param ju ArrayList con los juegos
      */
-    public static void buscarPorPlatadorma(ArrayList<Juego> ju){
+    public static void buscarPorPlatadorma(){
         String nombre=IntroducirDatos.introducirString("Nombre de la plataforma");
-        for(int i=0; i<ju.size(); i++){
-            if(nombre.equalsIgnoreCase(ju.get(i).getPlataforma().getNombre())){
-                System.out.println(ju.get(i).toString());
+        for(int i=0; i<juego.size(); i++){
+            if(nombre.equalsIgnoreCase(juego.get(i).getPlataforma().getNombre())){
+                System.out.println(juego.get(i).toString());
             }
         }
     }
@@ -89,9 +99,8 @@ public class FuncionesDirectorio{
      *
      * Este metodo añade objetos de tipo plataforma a un ArrayList
      *
-     * @param plataforma nombre del ArrayList
      */
-    public static void añadirPlataforma(ArrayList<Plataforma> plataforma){
+    public static void añadirPlataforma(){
         String nom=IntroducirDatos.introducirString("Nombre de la plataforma");
         String tipo=IntroducirDatos.introducirString("Que tipo de consola tienes");
         String des=IntroducirDatos.introducirString("Introduce una pequeña descripción");
@@ -106,9 +115,8 @@ public class FuncionesDirectorio{
      * Este metodo muestra todas las plataformas que se encuentren en el
      * ArrayList
      *
-     * @param plataforma
      */
-    public static void mostrarPlataformas(ArrayList<Plataforma> plataforma){
+    public static void mostrarPlataformas(){
         for(int i=0; i<plataforma.size(); i++){
             System.out.println(plataforma.get(i).toString());
         }
@@ -121,9 +129,8 @@ public class FuncionesDirectorio{
      * Este metodo pide un año y ese lo compara en la coleccion para así mostrar
      * los que coincidan.
      *
-     * @param plataforma
      */
-    public static void buscarPlataformaAñoSalida(ArrayList<Plataforma> plataforma){
+    public static void buscarPlataformaAñoSalida(){
         int año=IntroducirDatos.introducirInt("Año de salida que quieres buscar");
         for(int i=0; i<plataforma.size(); i++){
             if(plataforma.get(i).getAñoDeSalida()==año){
@@ -140,9 +147,8 @@ public class FuncionesDirectorio{
      *
      * Este metodo muestra las plataformas que coincidan con el nombre indicado.
      *
-     * @param plataforma
      */
-    public static void buscarPlataformaPorNombre(ArrayList<Plataforma> plataforma){
+    public static void buscarPlataformaPorNombre(){
         String nombre=IntroducirDatos.introducirString("Nombre de la Plataforma");
         for(int i=0; i<plataforma.size(); i++){
             if(plataforma.get(i).getNombre().equalsIgnoreCase(nombre)){
@@ -159,9 +165,8 @@ public class FuncionesDirectorio{
      *
      * Este método muestra las plataformas que coincidan con el tipo indicado.
      *
-     * @param plataforma
      */
-    public static void buscarPlataformaPorTipo(ArrayList<Plataforma> plataforma){
+    public static void buscarPlataformaPorTipo(){
         String tipo=IntroducirDatos.introducirString("Modelo de la Plataforma");
         for(int i=0; i<plataforma.size(); i++){
             if(plataforma.get(i).getTipo().equalsIgnoreCase(tipo)){
@@ -174,15 +179,12 @@ public class FuncionesDirectorio{
     }
 
     /**
-     * Metodo selecionar Plataforma.
-     *
      * Este metodo mediante la introduccion de un tipo de consola devuelve un
      * objeto de tipo Plataforma.
      *
-     * @param plataforma
-     * @return
+     * @return Objeto de tipo plataforma
      */
-    public static Plataforma selectPlataforma(ArrayList<Plataforma> plataforma){
+    public static Plataforma selectPlataforma(){
         String tipo=IntroducirDatos.introducirString("Introduce un model el modelo de consola");
         int i;
         for(i=0; i<plataforma.size(); i++){
@@ -196,14 +198,11 @@ public class FuncionesDirectorio{
     }
 
     /**
-     * Metodo buscar numero de jugadores.
+     * Metodo buscar numero de jugadores. Este metodo compara un numero
+     * introducido con el numero de jugadores de los juegos registrados.
      *
-     * Este metodo compara un numero introducido con el numero de jugadores de
-     * los juegos registrados.
-     *
-     * @param juego
      */
-    public static void buscarNumJugadores(ArrayList<Juego> juego){
+    public static void buscarNumJugadores(){
         int njugadores=IntroducirDatos.introducirInt("Introduce número de jugadores.");
         for(int i=0; i<juego.size(); i++){
             if(juego.get(i).getNjugadores()==njugadores){
@@ -214,29 +213,24 @@ public class FuncionesDirectorio{
     }
 
     /**
-     * Metodo para buscar los metodos por su año de
-     * lazamiento
+     * Metodo para buscar los metodos por su año de lazamiento
      *
-     * @param ju ArrayList con los juegos
      */
-    public static void buscarAñoLanz(ArrayList<Juego> ju){
+    public static void buscarAñoLanz(){
         int año=IntroducirDatos.introducirInt("Año de lanzamiento del juego buscar?");
-        for(int i=0; i<ju.size(); i++){
-            if(ju.get(i).getAñoLanzamiento()==año){
-                System.out.println(ju.get(i).toString());
+        for(int i=0; i<juego.size(); i++){
+            if(juego.get(i).getAñoLanzamiento()==año){
+                System.out.println(juego.get(i).toString());
             }
         }
     }
 
     /**
-     * Metodo buscar por dlc .
-     *
-     * Este metodo compara un boolean introducido con el valor dlc de juegos .
-     *
-     * @param juego
+     * Metodo buscar por dlc . Este metodo compara un boolean introducido con el
+     * valor dlc de juegos .
      *
      */
-    public static void buscarCoop(ArrayList<Juego> juego){
+    public static void buscarCoop(){
         Boolean coop=IntroducirDatos.introducirBoolean("Tiene cooperativo");
         for(int i=0; i<juego.size(); i++){
             if(juego.get(i).isCo_op()==coop){
@@ -251,9 +245,8 @@ public class FuncionesDirectorio{
      * En este metodo se compara un valor boolean con el metodo isTerminado de
      * la clase Juego
      *
-     * @param juego
      */
-    public static void buscarPorTerminado(ArrayList<Juego> juego){
+    public static void buscarPorTerminado(){
 
         Boolean terminado=IntroducirDatos.introducirBoolean("Tienes acabado el juego");
         if(terminado==true){
@@ -278,9 +271,8 @@ public class FuncionesDirectorio{
      * En este metodo se compara un valor boolean con el metodo isDlc de la
      * clase Juego
      *
-     * @param juego
      */
-    public static void buscarPorDLC(ArrayList<Juego> juego){
+    public static void buscarPorDLC(){
 
         Boolean dlc=IntroducirDatos.introducirBoolean("Tienes acabado el juego");
         if(dlc==true){
@@ -298,19 +290,20 @@ public class FuncionesDirectorio{
             }
         }
     }
+
     /**
      * Metodo de modificar terminado
-     * 
+     *
      * Este metodo añade un valor true al atributo terminado.
-     * @param juego 
+     *
      */
-    public static void modificarTerminado(ArrayList<Juego> juego){
+    public static void modificarTerminado(){
         String titulo=IntroducirDatos.introducirString("Introduce el titulo del juego");
         for(int i=0; i<juego.size(); i++){
-                if(juego.get(i).getTitulo().equalsIgnoreCase(titulo)){
-                    juego.get(i).setTerminado(true);
-                }
+            if(juego.get(i).getTitulo().equalsIgnoreCase(titulo)){
+                juego.get(i).setTerminado(true);
             }
-        
+        }
+
     }
 }
