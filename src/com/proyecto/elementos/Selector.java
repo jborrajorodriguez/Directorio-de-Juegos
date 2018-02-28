878d4698f4a00c4c2651996993892f92a0fb2e3e
arturo <arturo@ArturoMSI>
2018-02-28 12:08:53 +0100

    Arreglados fallos en los selectores

--- src/com/proyecto/elementos/Selector.java
+++ src/com/proyecto/elementos/Selector.java
@@ -9,12 +9,13 @@ import com.proyecto.display.Display;
  *
  * @author Juan Borrajo Rodriguez Nº5937 y Arturo Alvarellos Blas Nº6037.
  */
-public class Selector {
+public class Selector{
 
     //Este es el selector maestro.
-    public void selector() {
+    public void selector(){
         int opcion;
-        do {
+        FuncionesDirectorio.crearFicheros();
+        do{
             opcion=IntroducirDatos.introducirInt("********** Menú **********\n\n"
                     +"*-1 : Mostrar los juegos de tu directorio\n"
                     +"*-2 : Mostrar las plataformas de tu directorio\n"
@@ -23,7 +24,7 @@ public class Selector {
                     +"*-5 : Añadir plataforma\n"
                     +"*-6 : Modificar terminado\n"
                     +"*-0 : Salir\n\n");
-            switch (opcion) {
+            switch(opcion){
                 case 1:
                     FuncionesDirectorio.mostrarJuegos();
                     break;
@@ -35,53 +36,53 @@ public class Selector {
                     break;
                 case 4:
                     FuncionesDirectorio.añadirJuego();
+                    FuncionesDirectorio.guardarJuegos();
                     break;
                 case 5:
                     FuncionesDirectorio.añadirPlataforma();
+                    FuncionesDirectorio.guardarPlatafomas();
                     break;
                 case 6:
                     FuncionesDirectorio.modificarTerminado();
+                    FuncionesDirectorio.guardarFicheros();
                     break;
-                case 0:break;
-                //default:
-                    //Display.mostrarMensaje("Esa opcion no es correcta.");
+                case 0:
+                    break;
+                default:
+                    Display.mostrarMensaje("Esa opcion no es correcta.");
             }
-        }
-        while (opcion!=0);
+        }while(opcion!=0);
 
     }
 
     //Este es el selector de busquedas.
-    public void selectorBuscar() {
+    public void selectorBuscar(){
         int opcion;
-        do {
+        do{
             opcion=IntroducirDatos.introducirInt("********** Elige la Opcion **********\n\n"
-                    +"*-1 : Busqueda por Juegos\n"
-                    +"*-2 : Busqueda por Plataformas\n"
-                    +"*-3 : Volver al menú anterior\n\n");
-            switch (opcion) {
+                    +"*-1 : Busqueda de Juegos\n"
+                    +"*-2 : Busqueda de Plataformas\n"
+                    +"*-0 : Volver al menú anterior\n\n");
+            switch(opcion){
                 case 1:
                     this.selectorBusquedaPorJuego();
                     break;
                 case 2:
                     this.selectorBusquedaPorPlataforma();
                     break;
-                case 3:this.selector();
-                break;
-                //case 0:break;
-
+                case 0:
+                    break;
                 default:
                     Display.mostrarMensaje("Esa opcion no es correcta.");
             }
-        }
-        while (opcion!=3);
+        }while(opcion!=0);
 
     }
 
     //Selector de busqueda por juegos.
-    public void selectorBusquedaPorJuego() {
+    public void selectorBusquedaPorJuego(){
         int opcion;
-        do {
+        do{
             opcion=IntroducirDatos.introducirInt("********** Busqueda por Juego **********\n\n"
                     +"*-1 : Busqueda por Titulo\n"
                     +"*-2 : Busqueda por Desarroladora\n"
@@ -91,8 +92,8 @@ public class Selector {
                     +"*-6 : Busqueda por CO-OP\n"
                     +"*-7 : Busqueda por Juego Terminado\n"
                     +"*-8 : Busqueda por Contenido descargable\n"
-                    +"*-9 : Volver al menú anterior\n\n");
-            switch (opcion) {
+                    +"*-0 : Volver al menú anterior\n\n");
+            switch(opcion){
                 case 1:
                     FuncionesDirectorio.buscarTitulo();
                     break;
@@ -117,27 +118,24 @@ public class Selector {
                 case 8:
                     FuncionesDirectorio.buscarPorDLC();
                     break;
-                case 9:this.selectorBuscar();
-                break;
-                case 0:break;
-
+                case 0:
+                    break;
                 default:
                     Display.mostrarMensaje("Esa opcion no es correcta.");
             }
-        }
-        while (opcion!=0);
+        }while(opcion!=0);
     }
 
     //Selector de busqueda por plataforma.
-    public void selectorBusquedaPorPlataforma() {
+    public void selectorBusquedaPorPlataforma(){
         int opcion;
-        do {
+        do{
             opcion=IntroducirDatos.introducirInt("********** Busqueda por Plataforma **********\n\n"
                     +"*-1 : Busqueda por Nombre\n"
                     +"*-2 : Busqueda por Año de Salida\n"
                     +"*-3 : Busqueda por Modelo de plataforma\n"
-                    +"*-4 : Volver al menú anterior\n\n");
-            switch (opcion) {
+                    +"*-0 : Volver al menú anterior\n\n");
+            switch(opcion){
                 case 1:
                     FuncionesDirectorio.buscarPlataformaPorNombre();
                     break;
@@ -147,17 +145,12 @@ public class Selector {
                 case 3:
                     FuncionesDirectorio.buscarPlataformaPorTipo();
                     break;
-                case 4:
-                    this.selectorBuscar();
-                    break;
                 case 0:
                     break;
-
                 default:
                     Display.mostrarMensaje("Esa opcion no es correcta.");
             }
-        }
-        while (opcion!=0);
+        }while(opcion!=0);
     }
 
 }
